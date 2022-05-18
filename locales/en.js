8ae5ef7687f63e8d88ea904d95c6f8f40f7b86bb
Alexey Y <alexey.yashenkov@gmail.com>
2022-05-18 17:24:23 +0300

    Create en.js

---
+++ locales/en.js
@@ -0,0 +1,19 @@
+// @ts-check
+
+export default {
+  translation: {
+    // BEGIN (write your solution here)
+    languages: {
+      en: 'English',
+      ru: 'Русский',
+    },
+    buttons: {
+      counter: {
+        count_one: '{{count}} click',
+        count_other: '{{count}} clicks',
+      },
+      reset: 'Reset',
+    },
+    // END
+  },
+};
