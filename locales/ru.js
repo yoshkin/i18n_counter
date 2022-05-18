48e4bc6663f38da3a4143c408eb39f7877f5a91d
Alexey Y <alexey.yashenkov@gmail.com>
2022-05-18 17:24:41 +0300

    Create ru.js

---
+++ locales/ru.js
@@ -0,0 +1,20 @@
+// @ts-check
+
+export default {
+  translation: {
+    // BEGIN (write your solution here)
+    languages: {
+      en: 'English',
+      ru: 'Русский',
+    },
+    buttons: {
+      counter: {
+        count_one: '{{count}} клик',
+        count_few: '{{count}} клика',
+        count_many: '{{count}} кликов',
+      },
+      reset: 'Сбросить',
+    },
+    // END
+  },
+};
